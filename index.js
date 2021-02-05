22a782ae65f38cbdf58d89e72ae4fb911f5a5628
Daniel Martorell <dani.martorell@gmail.com>
2021-02-05 07:27:05 -0600

    addNums to display and multiple comma avoiding

--- index.js
+++ index.js
@@ -1,219 +1,34 @@
-// TODOS ******
 
-// BOTÓN DE +/-
-// USO DEL %   ??
+let currentOperationValue = '';
+let previousOperationValue = '';
+let currentOperationDisplay = document.querySelector('.current-op');
+let previousOperationDisplay = document.querySelector('.previous-op');
 
+const numKeys = document.querySelectorAll('.number');
 
-const OPERATORS = new Set([  '/', '*', '+', '-', '%' ]); 
-const OPERANDS = new Set([ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ]);
-const SPECIAL_KEYS = new Set([ 'Enter', 'Backspace', '=', '.', ',' ]);
-const calculate = {
-    '+' : (a,b) => a + b,
-    '-' : (a,b) => a - b,
-    '*' : (a,b) => a * b, 
-    '/' : (a,b) => a / b,
-    '%' : (a) => a / 100, 
+numKeys.forEach(key => key.addEventListener('click', addNumToCurrent));
+
+function clear(){
+    currentOperationValue = '';
+    previousOperationValue = '';
 }
 
-const main = document.querySelector('#main');
-
-let stringFromUser = '';
-let lastResult;
-let pointCounter = 0;
-let result;
-
-window.addEventListener('click', computeInput);
-
-window.addEventListener('keydown', computeInput);
-
-
-function computeInput(e){
-    const value = e.key || e.target.value;
-    let lastChar = stringFromUser.charAt(stringFromUser.length - 1);
-    if(isValid(value, lastChar, pointCounter)){
-        
-        switch(value){
-            
-            case ',':
-            case '.':
-                if(stringFromUser === 0 && main.textContent === '0'){
-                    stringFromUser += '0.';
-                    main.textContent = '0.'
-                } else if(!stringFromUser && main.textContent.length > 0){
-                    stringFromUser += '0.';
-                    main.textContent = '0.'
-                }else if(OPERATORS.has(lastChar)){
-                    stringFromUser += '0.';
-                    main.textContent += '0.';
-
-                } else {
-                    stringFromUser += '.';
-                    main.textContent += '.';
-                }
-                pointCounter = 1;
-                
-            break;
-
-            case '*':
-                if(!stringFromUser){
-                    stringFromUser += result ? `${result}*` : '0*';
-                    main.textContent = stringFromUser.replace('*', ' x ');
-                    pointCounter = 0;
-                } else {
-                    stringFromUser += '*';
-                    main.textContent += ' x ';
-                    pointCounter = 0;
-                }
-                
-            break;
-            case '/':
-
-                if(!stringFromUser){
-                    stringFromUser += result ? `${result}/` : '0/';
-                    main.textContent = stringFromUser.replace('/', ' ÷ ');
-                    pointCounter = 0;
-                } else {
-                    stringFromUser += '/';
-                    main.textContent += ' ÷ ';
-                    pointCounter = 0;
-                }
-            
-            break;
-            case '+':
-            
-                if(!stringFromUser){
-                    stringFromUser += result ? `${result}+` : '0+';
-                    main.textContent = stringFromUser.replace('+', ' + ');
-                    pointCounter = 0;
-                } else {
-                    stringFromUser += '+';
-                    main.textContent += ' + ';
-                    pointCounter = 0;
-                }
-
-            break;
-            case '-':
-
-                if(!stringFromUser){
-                    stringFromUser += result ? `${result}-` : '0-';
-                    main.textContent = stringFromUser.replace('-', ' - ');
-                    pointCounter = 0;
-                } else {
-                    stringFromUser += '-';
-                    main.textContent += ' - ';
-                    pointCounter = 0;
-                }
-            
-            break;
-            case '%':
-
-                if(!stringFromUser){
-                    stringFromUser += result ? `${result}%` : '0%';
-                    main.textContent = stringFromUser.replace('%', ' % ');
-                    pointCounter = 0;
-                } else {
-                    stringFromUser += '%';
-                    main.textContent += ' % ';
-                    pointCounter = 0;
-                }
-            
-            break;
-
-            case 'Backspace':
-               
-                if(stringFromUser.length <= 1){
-                    screenToZero();
-                    resetStringFromUser();
-                    resetResult();
-                    pointCounter = 0;
-                }
-                else {
-                    if(lastChar === '.'){
-                        pointCounter = 0;
-                    } 
-                    if(OPERATORS.has(lastChar)){
-                        main.textContent = main.textContent.slice(0,-2);
-                    }
-
-                    stringFromUser = stringFromUser.slice(0,-1);
-                    main.textContent = main.textContent.slice(0,-1);
-                }
-            break;
-            
-            case 'Enter':
-                //////////// ENTER KEY or =
-
-            if(!stringFromUser) {  // usuario aprieta intro sobre un resultado ya dado.
-                main.textContent = result || '0';   // Sin la segunda opción, si el usuario pulsa intro sobre el 0 inicial este desaparece
-                pointCounter = 0;
-            } 
-        
-            else if(stringFromUser){
-                const parsedString = parseString(stringFromUser);
-                console.log(parsedString);
-                result = processParsedString (parsedString);
-                console.log({result});
-                stringFromUser = '';
-                main.textContent = result;
-                // lastResult = result;
-                pointCounter = 0;
-            }
-    
-            break;
-            
-            default:
-                
-            if(!stringFromUser){
-                stringFromUser += value;
-                main.textContent = value;
-            }
-            else {
-                stringFromUser += value;
-                main.textContent += value;
-            }
-
-        }
+function addNumToCurrent(key){
+    const num = key.target.textContent;
+    if(num === '.' && currentOperationValue.includes('.')){
+        return;
+    } else {
+        currentOperationValue += key.target.textContent;
+        updateScreen(currentOperationValue);
     }
-};
+}
+function updateScreen(value){
+    currentOperationDisplay.textContent = value;
 
-function resetStringFromUser(){
-    return stringFromUser = '';
 }
 
-function resetResult(){
-    return result = 0;
-}
 
-function screenToZero(){
-    return main.textContent = 0;
-}
-
-function clearScreen(){
-    main.textContent = 0;
-    return;
-}
-
-function isEnter(key){
-    return key === 'Enter' || key === '='
-}
-
-function isValid(key, lastChar, pointCounter){
-
-    if (!OPERANDS.has(key) && !OPERATORS.has(key) && !SPECIAL_KEYS.has(key)) return false;
-    if ((key === ',' || key === '.') && pointCounter > 0) return false; 
-    if (lastChar !== '%' && OPERATORS.has(lastChar) && key === 'Enter') return false;
-
-    
-    if (key === '*' && lastChar === '*') return false;
-    if (key === '/' && lastChar === '/') return false;
-    if (key === '+' && lastChar === '+') return false;
-    if (key === '-' && lastChar === '-') return false;
-    if (key === '%' && lastChar === '%') return false;
-
-    return true;
-}
-
-function parseString(str){
+/*function parseString(str){
 
     const regex = /(?<operand>Infinity|[+\-]?\d*\.?\d+)(?<operator>[/*+\-%])?/g;
     let matches = [];
@@ -240,4 +55,4 @@ function processParsedString(parsedArray){
     }
     total = isNaN(total) ? 'Error' : total;
     return total;
-}
+}*/
