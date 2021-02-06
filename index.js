a6a85b806ade6203cf2805e64fb4f89be7a4c9a2
Daniel Martorell <dani.martorell@gmail.com>
2021-02-06 16:47:43 -0600

    si usuario intenta apretar '=' después de usar '0.' no devuelve nada.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 
-// TODOS
+// TODOS:
 // format números (separación con puntos y comas) : Intl.NumberFormat
 // Entrada input por teclado;
 
 let currentOperationValue = '';
 let previousOperationValue = '';
-let currentOperationDisplay = document.querySelector('.current-op');
-let previousOperationDisplay = document.querySelector('.previous-op');
 let currentOperator = '';
 let isEqualsLastKey = false;
+
+let currentOperationDisplay = document.querySelector('.current-op');
+let previousOperationDisplay = document.querySelector('.previous-op');
 
 const clearKey = document.querySelector('.clear');
 const delKey = document.querySelector('.delete');
@@ -24,31 +25,11 @@ numberKeys.forEach(key => key.addEventListener('click', ()=> {
 
 operatorKeys.forEach(key => key.addEventListener('click', () => {
     isEqualsLastKey = false;
-    if(!currentOperationValue && !previousOperationValue) return;
-    if(currentOperationValue === 'Error') clear();
-    if(!currentOperationValue){
-
-        currentOperator = key.textContent;
-        updateScreen();
-        return;
-    } 
-    if(currentOperationValue === '.') return;
-    if(previousOperationValue){
-        let result = compute();
-        previousOperationValue = result;
-        currentOperationValue = '';
-        currentOperator = key.textContent;
-        updateScreen();
-
-    } else {
-    currentOperator = key.textContent;
-    previousOperationValue = currentOperationValue;
-    currentOperationValue = '';
-    updateScreen();
-    }
+    addOperator(key);
 }));
 
 clearKey.addEventListener('click', clear);
+
 delKey.addEventListener('click', () => {
     if(currentOperationValue && !isEqualsLastKey){
         currentOperationValue = currentOperationValue.slice(0,-1);
@@ -60,6 +41,10 @@ delKey.addEventListener('click', () => {
 
 equalsKey.addEventListener('click', ()=> {
     isEqualsLastKey = true;
+    if(currentOperationValue === '0.'){
+        isEqualsLastKey = false;
+        return;
+    }
     if(!currentOperationValue) {
         isEqualsLastKey = false;
         return;
@@ -100,6 +85,31 @@ function compute(){
     result = +result.toFixed(2);
     return result.toString();
 }
+
+function addOperator(key){
+    if(!currentOperationValue && !previousOperationValue || currentOperationValue === '0.') return;
+    if(currentOperationValue === 'Error') clear();
+    if(!currentOperationValue){
+
+        currentOperator = key.textContent;
+        updateScreen();
+        return;
+    } 
+    if(previousOperationValue){
+        let result = compute();
+        previousOperationValue = result;
+        currentOperationValue = '';
+        currentOperator = key.textContent;
+        updateScreen();
+
+    } else {
+    currentOperator = key.textContent;
+    previousOperationValue = currentOperationValue;
+    currentOperationValue = '';
+    updateScreen();
+    }
+}
+
 function clear(){
     currentOperationValue = '';
     previousOperationValue = '';
@@ -119,36 +129,8 @@ function addNumToCurrent(key){
         updateScreen();
     }
 }
+
 function updateScreen(){
     currentOperationDisplay.textContent = currentOperationValue;
     previousOperationDisplay.textContent = `${previousOperationValue} ${currentOperator}`;
 }
-
-/*function parseString(str){
-
-    const regex = /(?<operand>Infinity|[+\-]?\d*\.?\d+)(?<operator>[/*+\-%])?/g;
-    let matches = [];
-    const output = [];
-    let match;
-    while(match = regex.exec(str)){ //el resultado del exec cambia en cada iteración. La última(no hay matches) es undefined y el while se rompe.
-        matches.push(match); 
-    }
-    for(let match of matches){
-        const { operand, operator } = match.groups;
-        output.push(operand, operator);
-    }
-    output
-    return output.filter(element => element);
-}
-
-function processParsedString(parsedArray){
-    let total = +parsedArray[0]
-
-    for(let i = 1; i < parsedArray.length; i += 2){
-        const operator = parsedArray[i];
-        const next = parsedArray[i+1];
-        total = +(calculate[operator](total,+next)).toFixed(10);
-    }
-    total = isNaN(total) ? 'Error' : total;
-    return total;
-}*/
